c0daa5d16706d12af3fb7f073db79677de6b58b6
kusut <tinokusut@gmail.com>
2012-10-30 15:06:58 +0700

    prep release

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
-version = '0.1'
 
-setup(name='loud',
-      version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='kusut',
-      author_email='kusut@dahsy.at',
-      url='http://bitbucket.org/kusut/loud',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      install_requires=["nose>=0.1.0"],
-      entry_points = {
-          'nose.plugins.0.10': [
-              'loud = loud:Loud'
-              ]
-          },
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
 
-      )
+setup(
+    name='loud',
+    version='0.1',
+    description="Nose plugin for sound notification",
+    long_description=README,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Testing",
+        "License :: MIT License",
+    ],
+    keywords='nose plugin testing audio',
+    author='kusut',
+    author_email='kusut@dahsy.at',
+    url='http://github.com/kusut/loud',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=["nose>=0.1.0"],
+    entry_points={
+        'nose.plugins.0.10': [
+            'loud = loud:Loud'
+        ]
+    },
+)
